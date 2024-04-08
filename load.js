454bb8b9305ca98f6ab0bc38546e952d450fdbf9
aBit <61603370+topanimator2@users.noreply.github.com>
2024-04-08 15:24:00 +0200

    Update load.js

--- load.js
+++ load.js
@@ -1,7 +1,10 @@
-let requestURL = "https://valorant-api.com/v1/playertitles";
+async function load(URL) {
+let requestURL = URL;
 const request = new Request(requestURL);
 const response = await fetch(request);
 const details = await response.json();
 details.array.forEach(element => {
     print(element)
-});
+})};
+
+load("https://valorant-api.com/v1/playertitles")
